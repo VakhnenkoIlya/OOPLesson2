d3bc7e05ba369a1b0ce4183d785d691001692234
VakhnenkoIlya <mazerfaker@list.ru>
2021-10-13 23:16:01 +0300

    exercise 2

---
+++ Lesson2/Lesson2/BankAccount.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2
+{
+    class BankAccount
+    {
+        private int number;
+        private double balance;
+        private Chek chek;
+        private static int nextnumber = 1;
+
+        private static void CreateNumber()
+        {
+            nextnumber++;
+        }
+        public BankAccount() 
+        {
+            this.number = nextnumber;
+            CreateNumber();
+        }
+        public int GetNumber()
+        {
+            return this.number;
+        }
+
+        public void SetNumber(int number)
+        {
+            this.number = number;
+        }
+
+        public double GetBalance()
+        {
+            return this.balance;
+        }
+        public void SetBalance(double balance)
+        {
+            this.balance = balance;
+        }
+    
+        public Chek GetChek()
+        {
+        return this.chek;
+        }
+
+        public void SetChek(Chek chek)
+        {
+        this.chek = chek;
+
+        }
+    }
+}
+    enum Chek
+    {
+        debit,
+        credit,
+        current
+    }
+
---
+++ Lesson2/Lesson2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lesson2
+{
+    class Program
+    {
+        static void Main()
+        {
+            BankAccount myAccount = new ();
+            myAccount.SetNumber(1);
+            myAccount.SetBalance(7899.99);
+            myAccount.SetChek(0);
+            Console.WriteLine($"Номер счета {myAccount.GetNumber()}, " +
+                $"Баланс {myAccount.GetBalance()}, " +
+                $"тип счета {myAccount.GetChek()}");
+            BankAccount myAccount2 = new();
+            myAccount2.SetBalance(350.90);
+            myAccount2.SetChek(Chek.current);
+            Console.WriteLine($"Номер счета {myAccount2.GetNumber()}, " +
+                $"Баланс {myAccount2.GetBalance()}, " +
+                $"тип счета {myAccount2.GetChek()}");
+        }
+    } 
+}
